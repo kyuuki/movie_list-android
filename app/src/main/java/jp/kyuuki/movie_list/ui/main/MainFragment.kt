e33a5ceb13b0426d42473f5bc08d787a66ceafbd
tanvimittal <tanvimittal2k@gmail.com>
2020-10-22 16:34:13 +0900

    - This is an example which takes you to netflix.

--- app/src/main/java/jp/kyuuki/movie_list/ui/main/MainFragment.kt
+++ app/src/main/java/jp/kyuuki/movie_list/ui/main/MainFragment.kt
@@ -1,12 +1,16 @@
 package jp.kyuuki.movie_list.ui.main
 
-import androidx.lifecycle.ViewModelProviders
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import jp.kyuuki.movie_list.R
+import kotlinx.android.synthetic.main.main_fragment.*
+
 
 class MainFragment : Fragment() {
 
@@ -18,11 +22,25 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+
+
         return inflater.inflate(R.layout.main_fragment, container, false)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        /*
+            This button being clicked takes you on netflix.
+         */
+        btnNetflix.setOnClickListener {
+            val netflix = Intent()
+            netflix.action = Intent.ACTION_VIEW
+            netflix.data = Uri.parse("http://www.netflix.com/watch/70153404")
+            netflix.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            activity!!.startActivity(netflix)
+        }
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
